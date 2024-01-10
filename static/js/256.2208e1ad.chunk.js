42d1bd18ddb56297ec4a8001098bcb95c7f7ff8b
Olha-buz <134137848+Olha-buz@users.noreply.github.com>
2024-01-10 22:12:21 +0000

    Deploying to gh-pages from @ Olha-buz/goit-react-hw-05-movies@29785a737ab3351d5b1d9200e01cbdec66721e1d 🚀

---
+++ static/js/256.2208e1ad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t);n(791);var o=n(689),r=n(184);t.default=function(){var e=(0,o.s0)();return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Not found"}),(0,r.jsx)("button",{type:"button",onClick:function(){e("/")},children:"Go Home"})]})}}}]);
+//# sourceMappingURL=256.2208e1ad.chunk.js.map
